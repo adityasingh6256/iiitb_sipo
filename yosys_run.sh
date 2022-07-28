04dc35d5730ef988007bc9d9d9a2d30b8b6f46d1
adityasingh6256 <110079790+adityasingh6256@users.noreply.github.com>
2022-07-28 10:36:55 +0530

    Add files via upload

---
+++ yosys_run.sh
@@ -0,0 +1,14 @@
+# read design
+
+read_verilog iiitb_sipo.v
+
+# generic synthesis
+synth -top iiitb_sipo
+
+# mapping to mycells.lib
+dfflibmap -liberty/home/aditya.singh/Desktop/iiitb_sipo/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/aditya.singh/Desktop/iiitb_sipo/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+clean
+flatten
+# write synthesized design
+write_verilog -assert synth_iiitb_sipo.v
